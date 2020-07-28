ad9f8f3988b24d45a597edf0e9659d5dae6cdfe1
Thauan Correa De Oliveira <tholiveira@id.uff.br>
2020-07-28 14:45:29 -0300

    Acrescentando o components o App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Menu from "./components/Header/index";
+import BannerMain from "./components/BannerMain/index";
+import Carousel from "./components/Carousel/index";
+import Footer from "./components/Footer/index";
 
 function App() {
   return <Menu />;
