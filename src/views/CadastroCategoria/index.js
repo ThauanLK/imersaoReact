1b05b2f14adcac00a5d80b896e66d23e45f9e719
Thauan Correa De Oliveira <tholiveira@id.uff.br>
2020-07-30 17:45:27 -0300

    Adicionando o compoente FormFIeld

--- src/views/CadastroCategoria/index.js
+++ src/views/CadastroCategoria/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Layout from "../../components/Layout/index";
+import FormField from "./components/FormField/index";
 
 function CadastroCategoria() {
   const defaultCategoryValues = {
@@ -12,53 +13,51 @@ function CadastroCategoria() {
   const [categories, setCategories] = useState([]);
   const [categoryValue, setCategoryValue] = useState(defaultCategoryValues);
 
-  const onChangeHandler = (eventName) => {
+  function onChangeHandler(eventName) {
     setValue(eventName.target.getAttribute("name"), eventName.target.value);
-  };
+  }
 
-  const setValue = (key, value) => {
+  function setValue(key, value) {
     setCategoryValue({ ...value, [key]: value });
-  };
+  }
 
   return (
     <Layout>
-      <h1>Nova Categoria </h1>
+      <h1>Página CadastroCategoria </h1>
       <p>Nome {categoryValue.name}</p>
       <p>Descrição {categoryValue.description}</p>
       <p>Cor {categoryValue.color}</p>
-      <Link to="/">Home</Link>
+
       <form
         onSubmit={(event) => {
           event.preventDefault();
           setCategories([...categories, categoryValue]);
-          setCategoryValue();
+          setCategoryValue(defaultCategoryValues);
         }}
       >
-        <div>
-          <label>
-            Nome da Categoria:
-            <input
-              type="text"
-              name="name"
-              value={categoryValue.name}
-              onChange={onChangeHandler}
-            />
-          </label>
-        </div>
-        <div>
-          <label>Descrição:</label>
-          <textarea></textarea>
-        </div>
-        <div>
-          <label>Cor:</label>
-          <input
-            type="color"
-            value={categoryValue.color}
-            name="color"
-            onChange={onChangeHandler}
-          />
-        </div>
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={categoryValue.name}
+          onChange={onChangeHandler}
+        />
 
+        <FormField
+          label="Descrição:"
+          type="????"
+          name="descricao"
+          value={categoryValue.description}
+          onChange={onChangeHandler}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={categoryValue.color}
+          onChange={onChangeHandler}
+        />
         <button>Cadastrar</button>
         <ul>
           {categories.map((categories, index) => {
